5e9920c7752ca72ab002f797cd9f8ffa9b359f2b
Michael Santos <michael.santos@gmail.com>
2020-02-17 09:26:16 -0500

    parser: fix parsing of PIDs
    PID float values were not accepted by the parser for some commands:
    
        signal 123.4 term
        reflow 123.4 10 20

--- src/runlet_parser.yrl
+++ src/runlet_parser.yrl
@@ -17,10 +17,16 @@ pipeline    -> filter '>' integer : ['$1'] ++ [{<<">">>, [unwrap('$3')]}].
 pipeline    -> filter : ['$1'].
 
 filter      -> command integer integer integer : {unwrap('$1'), [unwrap('$2'), unwrap('$3'), unwrap('$4')]}.
+filter      -> command float integer integer : {unwrap('$1'), [unwrap('$2'), unwrap('$3'), unwrap('$4')]}.
+filter      -> command string integer integer : {unwrap('$1'), [unwrap('$2'), unwrap('$3'), unwrap('$4')]}.
+
 filter      -> command integer integer : {unwrap('$1'), [unwrap('$2'), unwrap('$3')]}.
 filter      -> command integer : {unwrap('$1'), [unwrap('$2')]}.
 filter      -> command float : {unwrap('$1'), [unwrap('$2')]}.
+
 filter      -> command integer string : {unwrap('$1'), [unwrap('$2'), unwrap('$3')]}.
+filter      -> command float string : {unwrap('$1'), [unwrap('$2'), unwrap('$3')]}.
+
 filter      -> command string integer : {unwrap('$1'), [unwrap('$2'), unwrap('$3')]}.
 filter      -> command string float : {unwrap('$1'), [unwrap('$2'), unwrap('$3')]}.
 filter      -> command string string : {unwrap('$1'), [unwrap('$2'), unwrap('$3')]}.
--- test/runlet_cli_test.exs
+++ test/runlet_cli_test.exs
@@ -5,12 +5,28 @@ defmodule RunletCLITest do
   test "parser: string used as command" do
     assert {:ok, [{:string, 1, 'a string'}], 1} = Runlet.CLI.lex("\"a string\"")
 
-    assert {:error, "syntax error before: \"a string\""} =
-             Runlet.CLI.parse("\"a string\"")
+    assert {:error, "syntax error before: \"a string\""} = Runlet.CLI.parse("\"a string\"")
   end
 
   test "parse: integer used as command" do
     assert {:ok, [{:integer, 1, 1}], 1} = Runlet.CLI.lex("1")
     assert {:error, "syntax error before: 1"} = Runlet.CLI.parse("1")
+  end
+
+  test "parse: commands" do
+    # command command
+    assert {:ok, [{"help", ["foo"]}]} = Runlet.CLI.parse(~S(help foo))
+    assert {:ok, [{"help", ["foo"]}]} = Runlet.CLI.parse(~S(help "foo"))
+
+    # command pid string
+    assert {:ok, [{"signal", [1, "TERM"]}]} = Runlet.CLI.parse(~S(signal 1 "TERM"))
+    assert {:ok, [{"signal", [1.0, "TERM"]}]} = Runlet.CLI.parse(~S(signal 1.0 "TERM"))
+
+    # command pid integer integer
+    assert {:ok, [{"reflow", [1, 10, 20]}]} = Runlet.CLI.parse(~S(reflow 1 10 20))
+    assert {:ok, [{"reflow", [1.1, 10, 20]}]} = Runlet.CLI.parse(~S(reflow 1.1 10 20))
+
+    assert {:ok, [{"reflow", ["1 1.1 2.2", 10, 20]}]} =
+             Runlet.CLI.parse(~S(reflow "1 1.1 2.2" 10 20))
   end
 end
