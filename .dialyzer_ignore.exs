1c3bec02e9116b0c658d637738be9b6f0e6a404a
Michael Santos <michael.santos@gmail.com>
2022-05-11 07:17:34 -0400

    dialyzer: update ignore list

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,32 +1,32 @@
 # lib/runlet/cmd/query.ex:96:no_return
 # The created anonymous function has no local return.
 # ________________________________________________________________________________
-# lib/runlet/cmd/query.ex:203:no_return
+# lib/runlet/cmd/query.ex:219:no_return
 # Function open/1 has no local return.
 # ________________________________________________________________________________
-# lib/runlet/cmd/query.ex:213:call
+# lib/runlet/cmd/query.ex:229:call
 # The function call will not succeed.
-#
+# 
 # :gun.open(
-#   string(),
-#   _port :: any(),
-#   _opt :: %{
-#     :connect_timeout => _,
-#     :http_opts => %{:content_handlers => [:gun_data_h | :gun_sse_h, ...]},
-#     :protocols => [:http, ...],
-#     :retry => 16_777_215,
-#     :retry_timeout => _
-#   }
+#     string(),
+#     _port :: any(),
+#     _opt :: %{
+#           :connect_timeout => _,
+#           :http_opts => %{:content_handlers => [:gun_data_h | :gun_sse_h, ...]},
+#           :protocols => [:http, ...],
+#           :retry => 3,
+#           :retry_timeout => _
+#         }
 # )
-#
+# 
 # breaks the contract
 # (:inet.hostname() | :inet.ip_address(), :inet.port_number(), opts()) ::
 #   {:ok, pid()} | {:error, any()}
-#
-# ________________________________________________________________________________
+# 
+#   ________________________________________________________________________________
 [
   # https://github.com/ninenines/gun/pull/242
-  {"lib/runlet/cmd/query.ex", :call, 213},
+  {"lib/runlet/cmd/query.ex", :call, 229},
   {"lib/runlet/cmd/query.ex", :no_return, 96},
-  {"lib/runlet/cmd/query.ex", :no_return, 203}
+  {"lib/runlet/cmd/query.ex", :no_return, 219}
 ]
