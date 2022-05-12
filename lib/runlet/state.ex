3ee70c73f4066577641dc30d4740d3f58eeff07e
Michael Santos <michael.santos@gmail.com>
2022-05-12 07:23:37 -0400

    Revert "Runlet.State.table: open file read-only"
    This reverts commit a3e25ee3fb5ee42a90ee9fc0f797ab4f280ffb22.
    
    All users of the dets table are required to use the same access mode.
    
    Tests fail:
    
    ```
    $ mix test --seed=795358 test/runlet_process_test.exs
    
       1) test Job control: percent encoded name (RunletProcessTest)
           test/runlet_process_test.exs:9
         ** (CaseClauseError) no case clause matching: {:error, :incompatible_arguments}
         code: run("runlet_process_@#$%^")
         stacktrace:
           (runlet 1.1.4) lib/runlet/state.ex:87: Runlet.State.table/3
           (runlet 1.1.4) lib/runlet/process.ex:125: Runlet.Process.all/1
           test/runlet_process_test.exs:42: RunletProcessTest.run/1
           test/runlet_process_test.exs:10: (test)
    
    Randomized with seed 795358
    ```
    
    In the test case, a command is issued to list all processes while another
    runlet is exiting, deleting the PID from the process table.
    
    To demonstrate:
    
    ```
    iex(1)> :dets.open_file(:foo, type: :set, access: :read_write
    )
    {:ok, :foo}
    iex(2)> :dets.open_file(:foo, type: :set, access: :read)
    {:error, :incompatible_arguments}
    iex(3)> :dets.close(:foo)
    :ok
    iex(4)> x = :dets.open_file(:foo, type: :set, access: :read)
    {:ok, :foo}
    iex(5)> :dets.open_file(:foo, type: :set, access: :read_write)
    {:error, :incompatible_arguments}
    ```

--- lib/runlet/state.ex
+++ lib/runlet/state.ex
@@ -57,18 +57,12 @@ defmodule Runlet.State do
 
   @spec open(binary, String.t(), String.t()) :: {:ok, atom} | {:error, any}
   defp open(dir, type, id) do
-    open(dir, type, id, :read_write)
-  end
-
-  @spec open(binary, String.t(), String.t(), :read | :read_write) ::
-          {:ok, atom} | {:error, any}
-  defp open(dir, type, id, access) do
     user = encode(id)
     table = :erlang.binary_to_atom("#{type}_#{user}", :latin1)
     dirname = Path.join([dir, user])
     File.mkdir_p!(dirname)
     filename = [dirname, type] |> Path.join() |> String.to_charlist()
-    :dets.open_file(table, type: :set, file: filename, access: access)
+    :dets.open_file(table, type: :set, file: filename)
   end
 
   @doc """
@@ -83,16 +77,10 @@ defmodule Runlet.State do
   @spec table(binary, String.t(), String.t()) :: [tuple]
   def table(dir, type, id) do
     user = encode(id)
-
-    case open(dir, type, user, :read) do
-      {:ok, db} ->
-        r = :dets.foldl(fn x, a -> [x | a] end, [], db)
-        _ = :dets.close(db)
-        r
-
-      {:error, {:file_error, _, :enoent}} ->
-        []
-    end
+    {:ok, db} = open(dir, type, user)
+    r = :dets.foldl(fn x, a -> [x | a] end, [], db)
+    _ = :dets.close(db)
+    r
   end
 
   @doc """
